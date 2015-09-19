8179d781b0dd3e9db8e9f50cfdf2d3767d7ef4be
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-09-19 19:45:16 -0400

    Simplify Registration

--- components/registration.ts
+++ components/registration.ts
@@ -9,15 +9,9 @@ import {Router} from 'angular2/router';
   templateUrl: 'components/registration.html'
 })
 export class Registration {
-  guestService: GuestService;
-  router: Router;
+  constructor(public guestService: GuestService, public router: Router) {}
 
-  constructor(guestService: GuestService, router: Router) {
-    this.guestService = guestService;
-    this.router = router;
-  }
-
-  addGuest(event, name: string, about: string) {
+  addGuest(event: Event, name: string, about: string) {
     event.preventDefault();
 
     this.guestService.add(name, about);
