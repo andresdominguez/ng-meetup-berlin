4a6af52285cbd489e5927be4cd113bfe05e10391
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-09-19 19:39:29 -0400

    Make the router "work"

--- hello-berlin-app.ts
+++ hello-berlin-app.ts
@@ -1,18 +1,35 @@
-import {Component, View, bootstrap} from 'angular2/angular2'
+import {Component, View, bind, bootstrap} from 'angular2/angular2'
 import {GuestService} from 'services/guest-service';
-import {Router} from 'angular2/router';
+//import {Router} from 'angular2/router';
+//import {  LocationStrategy, HashLocationStrategy, HTT } from 'angular2/router';
+//import {ROUTER_BINDINGS} from 'angular2/router';
+//import {RouteConfig, RouterOutlet, RouterLink, ROUTER_BINDINGS, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Registration} from 'components/registration';
 
+import { ROUTER_BINDINGS, LocationStrategy, HashLocationStrategy, RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
+
+
+@RouteConfig([
+  {path: '/', as: 'registration', component: Registration},
+  //{path: '/list', as: 'list', component: Registration},
+  //{path: '/list', as: 'list', component: GuestList},
+  //{path: '/search', as: 'search', component: GuestSearch}
+])
 @Component({
-  selector: 'hello-berlin'
+  selector: 'hello-berlin',
 })
 @View({
-  templateUrl: 'hello-berlin.html'
+  templateUrl: 'hello-berlin.html',
+  directives: [ROUTER_DIRECTIVES]
 })
 class HelloBerlin {
   constructor() {
   }
 }
 
+
 bootstrap(HelloBerlin, [
-  GuestService
+  GuestService,
+  ROUTER_BINDINGS,
+  bind(LocationStrategy).toClass(HashLocationStrategy)
 ]);
